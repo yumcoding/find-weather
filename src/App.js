f6ec5625387059b6277d074204273ef4ac36d498
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-09-07 12:01:59 +0200

    getWeather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    
-  );
+class App extends Component {
+  state = {
+    city: "",
+    temp: "",
+    maxTemp: "",
+    minTemp: "",
+    weather: "",
+    description: "",
+    icon: "",
+  };
+
+  getWeather = async () => {
+    const API_KEY = "8f667f21fc1b75023d24daa366a24cbb";
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&appid=${API_KEY}`
+    );
+    const { name } = data;
+    const { temp, temp_max, temp_min } = data.main;
+    const { main, description, icon } = data.weather[0];
+    this.setState({
+      city: name,
+      temp,
+      maxTemp: temp_max,
+      minTemp: temp_min,
+      weather: main,
+      description,
+      icon,
+    });
+  };
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
+  render() {
+    return <div>Sanity Check</div>;
+  }
 }
 
 export default App;
