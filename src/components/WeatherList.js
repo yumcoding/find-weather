6eabf4f4dc76e39224912bdbf13d88b246a0ae70
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-09-07 16:51:58 +0200

    create WeatherList component

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Weather from "./Weather";
 
 class WeatherList extends Component {
+  static defaultProps = {
+    favCities: [],
+  };
+
   render() {
     const { favCities } = this.props;
-    console.log(favCities);
-    return <div>Sanity Check</div>;
+    const list = favCities.map((city, i) => <Weather key={i} city={city} />);
+    return <ul>{list}</ul>;
   }
 }
 
