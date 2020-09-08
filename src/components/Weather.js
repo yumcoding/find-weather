1c5ab248b5d5186e7c0f9c4994a72bcdddb6498f
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-09-08 10:59:11 +0200

    add Button and Modal to Weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Button from "./Button";
+import Modal from "./Modal";
 
 class Weather extends Component {
   state = {
     data: {
       city: "",
       temp: "",
+      maxTemp: "",
+      minTemp: "",
       weather: "",
+      description: "",
       icon: "",
     },
+    showing: false,
+  };
+
+  showModal = () => {
+    this.setState({ showing: true });
+  };
+
+  hideModal = () => {
+    this.setState({ showing: false });
   };
 
   getWeather = async (city) => {
@@ -17,13 +31,16 @@ class Weather extends Component {
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
     const { name } = data;
-    const { temp } = data.main;
-    const { main, icon } = data.weather[0];
+    const { temp, temp_max, temp_min } = data.main;
+    const { main, description, icon } = data.weather[0];
     this.setState({
       data: {
         city: name,
         temp,
+        maxTemp: temp_max,
+        minTemp: temp_min,
         weather: main,
+        description,
         icon,
       },
     });
@@ -34,15 +51,26 @@ class Weather extends Component {
   }
 
   render() {
-    const { city, temp, weather, icon } = this.state.data;
-    const weatherIcon = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const { data } = this.state;
+    const { showing } = this.state;
+    const weatherIcon = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
 
     return (
       <li className="fav-weather">
-        <h2 className="fav-city">{city}</h2>
-        <p className="fav-des">{weather}</p>
-        <img src={weatherIcon} alt={weather} />
-        <h1 className="fav-temp">{temp}℃</h1>
+        <h2 className="fav-city">{data.city}</h2>
+        <p className="fav-des">{data.weather}</p>
+        <img src={weatherIcon} alt={data.weather} />
+        <h1 className="fav-temp">{data.temp}℃</h1>
+        <Button showModal={this.showModal} />
+        {showing ? (
+          <Modal
+            data={this.state.data}
+            modalState={showing}
+            hideModal={this.hideModal}
+          />
+        ) : (
+          ""
+        )}
       </li>
     );
   }
